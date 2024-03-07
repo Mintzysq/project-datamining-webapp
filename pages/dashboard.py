f022a3658e62b6da80c2b9eb46f152f28eafd921
LT <1425.lt@gmail.com>
2024-03-07 22:58:13 +0700

    init

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -135,7 +135,7 @@ if data is not None:
             else:
                 raise e
 
-
+'''
 if data is not None:
         if not numeric_data.empty:
             corr_matrix = numeric_data.corr()
@@ -145,4 +145,4 @@ if data is not None:
             st.subheader("Correlation Matrix", divider='blue')
             st.write("Correlation between numeric variables:")
             st.write(corr_matrix.style.background_gradient(cmap='viridis'))
-
+'''
