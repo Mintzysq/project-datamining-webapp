cfbea2dfb8ed3ecd89d9c117ade264e29b9b3381
LT <1425.lt@gmail.com>
2024-03-07 22:50:36 +0700

    init

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib as plt
+import matplotlib 
 from PIL import Image 
 
 st.set_page_config(page_title="Dashbord",
